d84f3e6ea79228f449ab9861a7cb3915b8e8b4a1
guochenghui <guochenghui@fenbi.com>
2021-12-10 15:18:30 +0800

    ADD: violence

--- src/violence/ReverseStack.java
+++ src/violence/ReverseStack.java
@@ -6,8 +6,44 @@
  */
 package violence;
 
+import java.util.Stack;
+
 /**
  * @author guochenghui
  */
 public class ReverseStack {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        reverse(stack);
+
+        while(!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+    }
+
+    public static void reverse(Stack<Integer> stack){
+        if(stack.isEmpty()){
+            return;
+        }
+        int tmp = f(stack);
+        reverse(stack);
+        stack.push(tmp);
+    }
+
+    public static int f(Stack<Integer> stack){
+        int i = stack.pop();
+        if(stack.isEmpty()){
+            return i;
+        }else {
+            int last = f(stack);
+            stack.push(i);
+            return last;
+        }
+    }
+
 }
